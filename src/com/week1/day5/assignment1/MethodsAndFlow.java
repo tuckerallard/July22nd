8c9dad561ee1356c059104bd7bb8d3825d293712
tuckerallard <tucker_allard@yahoo.com>
2016-07-22 16:06:26 -0400

    07/22/2016 4:07pm

--- src/com/week1/day5/assignment1/MethodsAndFlow.java
+++ src/com/week1/day5/assignment1/MethodsAndFlow.java
@@ -1,5 +1,40 @@
 package com.week1.day5.assignment1;
 
-public class MethodsAndFlow {
+public class MethodsAndFlow 
+{
+	private String[] stringArray;
+
+	public MethodsAndFlow(String[] stringArray) 
+	{
+		this.stringArray = stringArray;
+	}
+	
+	public void outputSubset(int startPosition, int numberOfValues)
+	{
+		if(startPosition >= 0 && startPosition < stringArray.length)
+		{
+			
+			if(startPosition + numofValues > stringArray.length)
+			{
+				
+			}
+			
+			for (int i = startPosition; i < (startPosition + numberOfValues); i++)
+			{	
+				System.out.println(stringArray[i]);
+			}
+			
+		}
+		
+		else
+		{
+			System.out.println("Invalid");
+		}
+	}
+	
+	public void outputAll()
+	{
+		outputSubset(0, stringArray.length);
+	}
 
 }
---
+++ src/com/week1/day5/assignment1/TestMethodsAndFlow.java
@@ -0,0 +1,17 @@
+package com.week1.day5.assignment1;
+
+public class TestMethodsAndFlow 
+{
+	private String string;
+	
+	
+	public static void main(String[] args) 
+	{
+		MethodsAndFlow duke = new MethodsAndFlow(args);
+		
+		duke.outputAll();
+
+		
+	}
+
+}
